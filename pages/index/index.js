e864ab1f7a4cc397e6855763ebe48c5ea9333a92
gpl2016 <guopengli2016@163.com>
2018-03-22 13:42:03 +0800

    创建仓库

---
+++ pages/index/index.js
@@ -0,0 +1,63 @@
+//index.js
+//获取应用实例
+
+Page({
+  data: {
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    imgUrls: [
+      "../../images/1.jpg",
+      "../../images/2.jpg",
+      "../../images/3.jpg",
+      "../../images/4.jpg"
+    ],
+    navs: []
+  },
+  
+  onLoad:function(options){
+    var page = this;
+    var navs = this.loadNavData();
+    page.setData({navs: navs});
+  },
+
+  navBtn: function(e) {
+    var id = e.currentTarget.id;
+    if (id == "0") {
+      wx.navigateTo({
+        url: '../index/map/map'
+      })
+    }
+  },
+
+  loadNavData: function() {
+    var navs = [];
+    var nav0 = new Object();
+    nav0.img = "../../images/map.png";
+    nav0.name = '校园地图';
+    navs[0] = nav0;
+
+    var nav1 = new Object();
+    nav1.img = '../../images/money.png';
+    nav1.name = '校园卡充值';
+    navs[1] = nav1;
+
+    var nav2 = new Object();
+    nav2.img = '../../images/report.png';
+    nav2.name = '四六级成绩';
+    navs[2] = nav2;
+
+    var nav3 = new Object();
+    nav3.img = '../../images/libarary.png';
+    nav3.name = '图书馆';
+    navs[3] = nav3;
+
+    var nav4 = new Object();
+    nav4.img = '../../images/exam.png';
+    nav4.name = '考试成绩';
+    navs[4] = nav4;
+
+    return navs;
+  }
+})
